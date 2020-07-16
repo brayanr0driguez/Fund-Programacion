c2e2b990ef12592c716bb2e9e7ed6c1c94665dda
brayanr0driguez <68034588+brayanr0driguez@users.noreply.github.com>
2020-07-15 21:34:31 -0500

    Create imc.cs

---
+++ imc.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+using System;
+
+namespace ej_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su peso en kg:");
+            double kg = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese su altura en metros:");
+            double m = double.Parse(Console.ReadLine());
+            double m2 = m * m;
+
+            double imc = kg / m2;
+            Console.WriteLine("su indice de masa corporal es: " + imc);
+
+            if (imc <= 18.5) {
+                Console.WriteLine("usted está bajo de peso!");
+            }
+            else if (18.6 <= imc && imc <= 24.9) {
+                Console.WriteLine("su peso está normal!");
+            }
+            else if (25 <= imc && imc <= 29.9) {
+                Console.WriteLine("usted tiene sobrepeso!");
+            }
+            else {
+                Console.WriteLine("usted tiene obsidad!!");
+            }
+        }
+    }
+}
