236c9a8a507804359e0d471d7fafa477085b4ac0
brayanr0driguez <68034588+brayanr0driguez@users.noreply.github.com>
2020-07-15 21:52:32 -0500

    Create ecuacion_2do_grado.cs

---
+++ ecuacion_2do_grado.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ej_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese a, b, c, uno por uno: ");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            double discrm = (b * b) - (4 * a * c);
+
+            if (discrm == 0)
+            {
+                double rspUnica = -1 * b / 2 * a;
+                Console.WriteLine("x: " + rspUnica);
+            }
+            else if (discrm > 0)
+            {
+                double x1 = ((-1 * b) + Math.Sqrt((b * b) - 4 * a * c)) / 2 * a;
+                double x2 = ((-1 * b) - Math.Sqrt((b * b) - 4 * a * c)) / 2 * a;
+
+                Console.WriteLine("x1: " + x1);
+                Console.WriteLine("x2: " + x2);
+            }
+            else
+            {
+                Console.WriteLine("no es posible solucionar este problema");
+            }
+        }
+    }
+}
