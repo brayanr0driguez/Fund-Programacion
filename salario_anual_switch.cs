d9fcf9e6ad36c0aeeb039be8e0c0e32a4b4cf933
brayanr0driguez <68034588+brayanr0driguez@users.noreply.github.com>
2020-07-24 10:19:17 -0500

    Update salario_anual_switch.cs

--- salario_anual_switch.cs
+++ salario_anual_switch.cs
@@ -17,60 +17,62 @@ namespace ej_9_eval
 
             double sal40 = salario * 0.40;
             int salMin = 877803;
-
-            if (dependencia == 1)
+            
+            switch(dependencia)
             {
-                if (sal40 < salMin)
+                case (1):
                 {
-                    deduccion1 = ((salMin * 0.04) * 2);
+                    if (sal40 < salMin)
+                    {
+                        deduccion1 = ((salMin * 0.04) * 2);
+
+                    }
+                    else
+                    {
+                        deduccion1 = ((sal40 * 0.04) * 2);
+                    }
+
+                    Console.WriteLine("deducción mensual: " + deduccion1);
+                    salMen1 = salario - deduccion1;
+                    Console.WriteLine("salario real mensual: " + salMen1);
+                    salAnual1 = (salMen1 * 12) + salario;
+                    Console.WriteLine("salario anual: " + salAnual1);
+                    break;
+                }
                     
-                }
-                else
+                case (2):
                 {
-                    deduccion1 = ((sal40 * 0.04) * 2);
-                }
+                    riesgo = 0;
+                    Console.WriteLine("cual es su clase de riesgo? (escriba el número correspondiente)");
+                    Console.WriteLine("| 1 | 2 | 3 | 4 | 5 |");
 
-                Console.WriteLine("deducción mensual: " + deduccion1);
-                salMen1 = salario - deduccion1;
-                Console.WriteLine("salario real mensual: " + salMen1);
-                salAnual1 = (salMen1 * 12) + salario;
-                Console.WriteLine("salario anual: " + salAnual1);
-            }
-            else if (dependencia != 1 && dependencia != 2)
-            {
-                Console.WriteLine("entrada incorrecta");
-            }
-            else
-            {
-                riesgo = 0;
-                Console.WriteLine("cual es su clase de riesgo? (escriba el número correspondiente)");
-                Console.WriteLine("| 1 | 2 | 3 | 4 | 5 |");
-                
-                switch(riesgo)
-                {
-                    case (1): riesgo = 0.00522; break;
-                    case (2): riesgo = 0.01044; break;
-                    case (3): riesgo = 0.02436; break;
-                    case (4): riesgo = 0.04350; break;
-                    case (5): riesgo = 0.06960; break;
-                    default: Console.WriteLine("número incorrecto"); break; 
-                }
+                    switch (riesgo)
+                    {
+                        case (1): riesgo = 0.00522; break;
+                        case (2): riesgo = 0.00522; break;
+                        case (3): riesgo = 0.00522; break;
+                        case (4): riesgo = 0.00522; break;
+                        case (5): riesgo = 0.00522; break;
+                        default: Console.WriteLine("número incorrecto"); break;
+                    }
 
-                if (sal40 < salMin)
-                {
-                    deduccion2 = (salMin * 0.16) + (salMin * 0.125) + (salMin * riesgo);
+                    if (sal40 < salMin)
+                    {
+                        deduccion2 = (salMin * 0.16) + (salMin * 0.125) + (salMin * riesgo);
 
-                }
-                else
-                {
-                    deduccion2 = (sal40 * 0.16) + (sal40 * 0.125) + (sal40 * riesgo);
-                }
+                    }
+                    else
+                    {
+                        deduccion2 = (sal40 * 0.16) + (sal40 * 0.125) + (sal40 * riesgo);
+                    }
 
-                Console.WriteLine("deducción mensual: " + deduccion2);
-                salMen2 = salario - deduccion2;
-                Console.WriteLine("salario real mensual: " + salMen2);
-                salAnual2 = salMen2 * 12;
-                Console.WriteLine("salario anual: " + salAnual2);
+                    Console.WriteLine("deducción mensual: " + deduccion2);
+                    salMen2 = salario - deduccion2;
+                    Console.WriteLine("salario real mensual: " + salMen2);
+                    salAnual2 = salMen2 * 12;
+                    Console.WriteLine("salario anual: " + salAnual2);
+                    break;
+                }
             }
         }
     }
